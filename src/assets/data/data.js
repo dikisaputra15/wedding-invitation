09011a304ba248242d43a16b359f13e2a35b97c8
Diki <catalina@Catalinas-MacBook-Air.local>
2024-11-03 20:55:10 +0700

    wedding invitation js

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -1,126 +1,127 @@
 export const data = {
-    bride: {
-        L: {
-            id: 1,
-            name: 'Lorem Ipsum',
-            child: 'Putra ke lorem',
-            father: 'Lorem',
-            mother: 'Ipsum',
-            image: './src/assets/images/cowo.png'
-        },
-        P: {
-            id: 2,
-            name: 'Ipsum Lorem',
-            child: 'Putri ke lorem',
-            father: 'Lorem',
-            mother: 'Ipsum',
-            image: './src/assets/images/cewe.png'
-        },
-
-        couple: './src/assets/images/couple.png'
+  bride: {
+    L: {
+      id: 1,
+      name: "Diki Saputra, S.Kom",
+      child: "Putra ke 2",
+      father: "Mistak",
+      mother: "Rubiah",
+      image: "./src/assets/images/cowo.png",
+    },
+    P: {
+      id: 2,
+      name: "Tuti Mulyasari, S.Ak",
+      child: "Putri ke 4",
+      father: "Sudin (Alm)",
+      mother: "Raspati",
+      image: "./src/assets/images/cewe.png",
     },
 
-    time: {
-        marriage: {
-            year: '2024',
-            month: 'November',
-            date: '14',
-            day: 'Kamis',
-            hours: {
-                start: '08.00',
-                finish: 'Selesai'
-            }
-        },
-        reception: {
-            year: '2024',
-            month: 'November',
-            date: '14',
-            day: 'Kamis',
-            hours: {
-                start: '11.00',
-                finish: 'Selesai'
-            }
-        },
-        address: 'Kp. Lorem, RT 000/ RW 000, Desa.Lorem, Kec.Ipsum, Kab.Lorem, Lorem (1234)'
+    couple: "./src/assets/images/couple.png",
+  },
+
+  time: {
+    marriage: {
+      year: "2024",
+      month: "November",
+      date: "09",
+      day: "Sabtu",
+      hours: {
+        start: "09.00",
+        finish: "Selesai",
+      },
     },
-
-    link: {
-        calendar: 'https://calendar.app.google/sZuPoeZejuhSmmbi7',
-        map: 'https://maps.app.goo.gl/q1Ask2Jgd4ekiiKBA',
+    reception: {
+      year: "2024",
+      month: "November",
+      date: "09",
+      day: "Sabtu",
+      hours: {
+        start: "09.00",
+        finish: "Selesai",
+      },
     },
+    address:
+      "Kp. Kembang RT 002 RW 006 Kelurahan Curug Kecamatan Curug Kota Serang - Banten",
+  },
 
-    galeri: [
-        {
-            id: 1,
-            image: './src/assets/images/1.png'
-        },
-        {
-            id: 2,
-            image: './src/assets/images/2.png'
-        },
-        {
-            id: 3,
-            image: './src/assets/images/3.png'
-        },
-        {
-            id: 4,
-            image: './src/assets/images/4.png'
-        },
-        {
-            id: 5,
-            image: './src/assets/images/5.png'
-        }
-    ],
+  link: {
+    calendar: "https://calendar.app.google/iRjLTPjBEDgw5wQH8",
+    map: "https://maps.app.goo.gl/3ntaRPpRBenruYcd7",
+  },
 
-    bank: [
-        {
-            id: 1,
-            name: 'Lorem Ipsum',
-            icon: './src/assets/images/bca.png',
-            rekening: '12345678'
-        },
-        {
-            id: 2,
-            name: 'Ipsum Lorem',
-            icon: './src/assets/images/bri.png',
-            rekening: '12345678'
-        },
-    ],
+  galeri: [
+    {
+      id: 1,
+      image: "./src/assets/images/1.png",
+    },
+    {
+      id: 2,
+      image: "./src/assets/images/2.png",
+    },
+    {
+      id: 3,
+      image: "./src/assets/images/3.png",
+    },
+    {
+      id: 4,
+      image: "./src/assets/images/4.png",
+    },
+    {
+      id: 5,
+      image: "./src/assets/images/5.png",
+    },
+  ],
 
-    audio: './src/assets/audio/wedding.mp3',
+  bank: [
+    {
+      id: 1,
+      name: "Tuti Mulyasari",
+      icon: "./src/assets/images/bca.png",
+      rekening: "5485172731",
+    },
+    {
+      id: 2,
+      name: "Diki Saputra",
+      icon: "./src/assets/images/mandiri.png",
+      rekening: "1630003521047",
+    },
+  ],
 
-    api: 'https://script.google.com/macros/s/AKfycbw20dzmJFBhCGIOMJXuKadQa9HQkABtLK47t-SWU9JMLeR25NiU5vF1Dxk0I3k_HmSZCQ/exec',
+  audio: "./src/assets/audio/wedding.mp3",
 
-    navbar: [
-        {
-            id: 1,
-            teks: 'Home',
-            icon: 'bx bxs-home-heart',
-            path: '#home',
-        },
-        {
-            id: 2,
-            teks: 'Mempelai',
-            icon: 'bx bxs-group',
-            path: '#bride',
-        },
-        {
-            id: 3,
-            teks: 'Tanggal',
-            icon: 'bx bxs-calendar-check',
-            path: '#time',
-        },
-        {
-            id: 4,
-            teks: 'Galeri',
-            icon: 'bx bxs-photo-album',
-            path: '#galeri',
-        },
-        {
-            id: 5,
-            teks: 'Ucapan',
-            icon: 'bx bxs-message-rounded-dots',
-            path: '#wishas',
-        },
-    ],
-}
+  api: "https://script.google.com/macros/s/AKfycbxPXakfVG24lt4KkALU5IAbAUqvibHKwTZ5dFHrTG9mY4UgNVtV_NzhIiGjWBZZLcJyxg/exec",
+
+  navbar: [
+    {
+      id: 1,
+      teks: "Home",
+      icon: "bx bxs-home-heart",
+      path: "#home",
+    },
+    {
+      id: 2,
+      teks: "Mempelai",
+      icon: "bx bxs-group",
+      path: "#bride",
+    },
+    {
+      id: 3,
+      teks: "Tanggal",
+      icon: "bx bxs-calendar-check",
+      path: "#time",
+    },
+    {
+      id: 4,
+      teks: "Galeri",
+      icon: "bx bxs-photo-album",
+      path: "#galeri",
+    },
+    {
+      id: 5,
+      teks: "Ucapan",
+      icon: "bx bxs-message-rounded-dots",
+      path: "#wishas",
+    },
+  ],
+};
